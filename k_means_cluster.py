86244ce0b836071c7ecb1f2d9325d3cbc098af25
HVS <vinovator@yahoo.co.in>
2018-02-25 17:14:53 +0530

    Add files via upload
    K Means clustering

---
+++ k_means_cluster.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python 
+
+""" 
+    Skeleton code for k-means clustering mini-project.
+"""
+
+
+
+
+import pickle
+import numpy
+import matplotlib.pyplot as plt
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+
+
+
+def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
+    """ some plotting code designed to help you visualize your clusters """
+
+    ### plot each cluster with a different color--add more colors for
+    ### drawing more than five clusters
+    colors = ["b", "c", "k", "m", "g"]
+    for ii, pp in enumerate(pred):
+        plt.scatter(features[ii][0], features[ii][1], color = colors[pred[ii]])
+
+    ### if you like, place red stars over points that are POIs (just for funsies)
+    if mark_poi:
+        for ii, pp in enumerate(pred):
+            if poi[ii]:
+                plt.scatter(features[ii][0], features[ii][1], color="r", marker="*")
+    plt.xlabel(f1_name)
+    plt.ylabel(f2_name)
+    plt.savefig(name)
+    plt.show()
+
+
+
+### load in the dict of dicts containing all the data on each person in the dataset
+data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") )
+### there's an outlier--remove it! 
+data_dict.pop("TOTAL", 0)
+
+
+### the input features we want to use 
+### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+feature_1 = "salary"
+feature_2 = "exercised_stock_options"
+# feature_3 = "total_payments"
+poi  = "poi"
+# features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
+data = featureFormat(data_dict, features_list )
+poi, finance_features = targetFeatureSplit( data )
+
+
+### in the "clustering with 3 features" part of the mini-project,
+### you'll want to change this line to 
+### for f1, f2, _ in finance_features:
+### (as it's currently written, the line below assumes 2 features)
+for f1, f2 in finance_features:
+    plt.scatter( f1, f2 )
+plt.show()
+
+### cluster here; create predictions of the cluster labels
+### for the data and store them to a list called pred
+from sklearn.cluster import KMeans
+
+# detault n_init=10; max_iter=300
+clusters = KMeans(n_clusters=2).fit(finance_features)
+pred = clusters.predict(finance_features)
+
+# get the max and min stock options paid out, ignoring NaN
+max_stock = max([value["exercised_stock_options"] for key, value in data_dict.items() 
+                        if value["exercised_stock_options"] != "NaN"])
+print("max stock: {0}".format(max_stock))
+print("max stock to : {0}".format([key for key, value in data_dict.items() 
+                            if value["exercised_stock_options"] == max_stock]))
+# 34348384 to LAY KENNETH L
+
+min_stock = min([value["exercised_stock_options"] for key, value in data_dict.items() 
+                        if value["exercised_stock_options"] != "NaN"])
+print("min stock: {0}".format(min_stock))
+print("min stock to : {0}".format([key for key, value in data_dict.items() 
+                            if value["exercised_stock_options"] == min_stock]))
+# 3285 to BELFER ROBERT
+    
+# get the max and min salary paid out, ignoring NaN
+max_salary = max([value["salary"] for key, value in data_dict.items() 
+                        if value["salary"] != "NaN"])
+print("max salary: {0}".format(max_salary))
+print("max salary to : {0}".format([key for key, value in data_dict.items() 
+                            if value["salary"] == max_salary]))
+# 1111258 to SKILLING JEFFREY K
+
+min_salary = min([value["salary"] for key, value in data_dict.items() 
+                        if value["salary"] != "NaN"])
+print("min salary: {0}".format(min_salary))
+print("min salary to : {0}".format([key for key, value in data_dict.items() 
+                            if value["salary"] == min_stock]))
+# 477 to NaN
+
+### rename the "name" parameter when you change the number of features
+### so that the figure gets saved to a different file
+try:
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+except NameError:
+    print ("no predictions object named pred found, no clusters to plot")
